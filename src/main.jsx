54570572f02b1a11317e0a29c0d9dad32f476673
galaders <kjetil.engvold@outlook.com>
2026-03-13 10:26:23 +0100

    siste fiks av notater skriving

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@
 // den, ble bedt av studasident å se over den error "thor"
 // bruk firefox for å se åsen jeg laget siden 
 // den ser grei ut på brave også men vet ikke om flere en de 2
-// fik hjelp av thor me å få started den opp etter første suret mit
-// på tirsdag og fik se over det jeg lurte på torsdag med han igjen.
+// fik hjelp av thor me å få started den opp etter første søking bit 
+// "finne navn på filmer på startsiden" på tirsdag 
+// og fik se over det jeg lurte på torsdag med han igjen.
 // det jeg fikk hjelp var at startsiden viste navn på 10 
 // james bond filmer og spill etter det klarte jeg å fikse det selv bedre
 // med litt Ai hjelp for vanskelig bitene jeg trengte hjelp med
